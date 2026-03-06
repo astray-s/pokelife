e50e0037a454600ddad015d163b559dd22956aca
Aathraey S <aathraey.shrikanth@gmail.com>
2026-03-06 10:56:36 -0800

    Add deployment configs and build production bundle

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,13 +6,13 @@ import { getDatabase } from 'firebase/database';
 // IMPORTANT: Replace these with your own Firebase project credentials
 // Get them from: https://console.firebase.google.com/
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBjCmlq5VdoNUPVdIs2WL_zf2RG4ybZLss",
+  authDomain: "pokelife-edf36.firebaseapp.com",
+  databaseURL: "https://pokelife-edf36-default-rtdb.firebaseio.com",
+  projectId: "pokelife-edf36",
+  storageBucket: "pokelife-edf36.firebasestorage.app",
+  messagingSenderId: "290602920362",
+  appId: "1:290602920362:web:d27e5f37324197431323d3"
 };
 
 // Initialize Firebase
