76302327bde367310905255353cca5241e85065c
Aathraey S <aathraey.shrikanth@gmail.com>
2026-03-07 08:53:10 -0800

    Update Firebase config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Replace with your actual Firebase config from Firebase Console
 // Go to: Firebase Console > Project Settings > Your apps > Firebase SDK snippet
 const firebaseConfig = {
-  apiKey: "AIzaSyBqVHWLxQxH8KZxQxH8KZxQxH8KZxQxH8",
-  authDomain: "synthpoke-default-rtdb.firebaseapp.com",
-  projectId: "synthpoke-default-rtdb",
-  storageBucket: "synthpoke-default-rtdb.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef"
+  apiKey: "AIzaSyBjCmlq5VdoNUPVdIs2WL_zf2RG4ybZLss",
+  authDomain: "pokelife-edf36.firebaseapp.com",
+  databaseURL: "https://pokelife-edf36-default-rtdb.firebaseio.com",
+  projectId: "pokelife-edf36",
+  storageBucket: "pokelife-edf36.firebasestorage.app",
+  messagingSenderId: "290602920362",
+  appId: "1:290602920362:web:d27e5f37324197431323d3"
 };
 
 const app = initializeApp(firebaseConfig);
