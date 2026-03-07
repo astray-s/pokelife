7c4486c2c180a8390c8da41debbf66464ba8e6c1
Aathraey S <aathraey.shrikanth@gmail.com>
2026-03-07 09:37:51 -0800

    Update Firebase configuration

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 // TODO: Replace with your actual Firebase config from Firebase Console
 // Go to: Firebase Console > Project Settings > Your apps > Firebase SDK snippet
 const firebaseConfig = {
-  apiKey: "AIzaSyBjCmlq5VdoNUPVdIs2WL_zf2RG4ybZLss",
-  authDomain: "pokelife-edf36.firebaseapp.com",
-  databaseURL: "https://pokelife-edf36-default-rtdb.firebaseio.com",
-  projectId: "pokelife-edf36",
-  storageBucket: "pokelife-edf36.firebasestorage.app",
-  messagingSenderId: "290602920362",
-  appId: "1:290602920362:web:d27e5f37324197431323d3"
+  apiKey: "AIzaSyCOXwkhABK5LtzKQS3ZyVBopXoq-GS5Ttg",
+  authDomain: "pokelife-tracker.firebaseapp.com",
+  projectId: "pokelife-tracker",
+  storageBucket: "pokelife-tracker.firebasestorage.app",
+  messagingSenderId: "748400307285",
+  appId: "1:748400307285:web:115387aefd7aec7b7c38dc",
+  measurementId: "G-RT6DC1R1LP"
 };
 
 console.log('Initializing Firebase with project:', firebaseConfig.projectId);
